a3ca3d3c1bab0659b4761d2d3c2c612e70b38140
Kelly <kelly.shpark@gmail.com>
2020-10-07 13:10:03 -0400

    readme, default

--- default.php
+++ default.php
@@ -7,7 +7,7 @@ require('header.php');
     <div class ="jumbotron" style="background-color: #e3f2fd;">
         <h1>Netflix Viewing Activity</h1>
         <p>This site was created for COMP1006 Intro to Web Programming in Fall 2018 at Georgian College.
-            It uses PHP, MySQL, and Twitter Bootstrap and is deployed to <b>AWS. -> Transferred to Heroku due to the expiration of AWS (2020). <b></p>
+            It uses PHP, MySQL, and Twitter Bootstrap and is deployed to <b>AWS. -> Transferred to Heroku (2020). <b></p>
         <p>This is for making Netflix Viewing Activities. Input what you watched, and give the rating/comment. </p>
         <p>Source Code is available at <a href="https://github.com/kellyshaprk/board_PHP-MySQL-Bootstrap" target="_blank"> https://github.com/kellyshaprk/board_PHP-MySQL-Bootstrap</a>.</p>
     </div>
