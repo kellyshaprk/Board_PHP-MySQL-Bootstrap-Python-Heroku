bddd44fdd1738959aa6f8acd70a111535ccaadfc
Kelly <kelly.shpark@gmail.com>
2020-10-13 23:59:54 -0400

    test

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ url = f"https://can.newonnetflix.info/catalogue/search/{search}#results"
 result = fetch(url)
 result = json.dumps(fetch(url), indent=4, sort_keys=True )
 result = result.replace("\"{title", "{\"title" + "\"").replace("\""+ "}" + "\"", "\""+ "}").replace("\\", "")
-result = "var py_result = " + result
+result = result
 
 py_result = open('py_result.json','w')
 py_result.write(result)
